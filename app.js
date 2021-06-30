d4ab2d4301ae123aadb300d41e802d45add39a13
Shariq0334 <khanshariq198@gmail.com>
2021-06-30 15:53:01 -0700

    Object Assign

---
+++ app.js
@@ -0,0 +1,64 @@
+// 1. Suppose You have an array of object
+// var itemsArray = [
+// {name:”juice”,price:”50”, quantity:”3”},
+// {name:”cookie”,price:”30”, quantity:”9”},
+// {name:”shirt”,price:”880”, quantity:”1”},
+// {name:”pen”,price:”100”, quantity:”2”}];
+// Calculate total price of each item and all items;
+
+
+// var itemsArray = [
+//     {name:'juice',price:'50', quantity:'3'},
+//     {name:'cookie',price:'30', quantity:'9'},
+//     {name:'shirt',price:'880', quantity:'1'},
+//     {name:'pen',price:'100', quantity:'2'}];
+
+//         itemsArray.forEach(function (item) {
+//             total = 0;
+//            let sum = item.price * item.quantity;
+//            console.log(sum);
+          
+           
+// }
+// );
+
+// 2. Create an object with properties name, email, password, age,
+// gender, city, country.
+// Check if age and country properties exist in object or not.
+// Also check firstName and lastName properties in object.
+
+// var obj = {
+//     name : "Shariq", 
+//     age : 87,
+//     email : "shasi",
+//     city : "Karachi",
+//     country : "Pakistan"
+// }
+// var check = "age" in obj
+// var check = "country" in obj
+// console.log(check)
+
+// Create a constructor function with some properties. Now
+// create multiple records using the constructor.
+
+// function data(brands,models,price){
+//     this.brands = ["Apple","Samsung","Motrolla"]
+
+//     this.models = ["Iphone4","Iphone5","Iphone6","Iphone6 Plus","Iphone 7", "Iphone7 plus","Iphone 7 s","Iphone 8","Iphone 8 plus","Iphone 9","Iphone X" ,"Iphone 11","Iphone ,12",
+//                        "Samsung Note 4","Samsung note 5", "Samsung Note 10"]
+//     this.price - [1,1,1,1,,1,,1,1,1,1,1,1,1,1,,1,1,1,1,1]
+    
+// }
+
+// 4. Suppose you want to check population of your area, their
+// educations and professions.
+// Create a constructor function which holds following
+// properties:
+// Name, gender, address, education, profession,
+// Enter all records one by one.
+// Hint:
+
+//  use select box for education and profession,
+//  use radio box for gender
+
+// Bonus : use can use localStorage to save records.
